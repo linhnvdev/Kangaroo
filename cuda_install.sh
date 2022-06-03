db7802e18e4f260ce30db5b25c60e5ae7ec8d60c
linhnvdev <106822026+linhnvdev@users.noreply.github.com>
2022-06-03 22:23:36 +0700

    Update cuda_install.sh

--- cuda_install.sh
+++ cuda_install.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 
-# Purge existign CUDA first
-sudo apt --purge remove "cublas*" "cuda*"
-sudo apt --purge remove "nvidia*"
+sudo apt install build-essential
+sudo apt-get install g++-4.8
+sudo apt-get install libpath-tiny-perl
 
-# Install CUDA Toolkit 10
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub && sudo apt update
-sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-
-sudo apt update
-sudo apt install -y cuda=10.0.130-1
-
-# Install CuDNN 7 and NCCL 2
-wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo dpkg -i nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-
-sudo apt update
-sudo apt install -y libcudnn7 libcudnn7-dev libnccl2 libc-ares-dev
-
-sudo apt autoremove
-# sudo apt upgrade
-
-# Link libraries to standard locations
-# sudo mkdir -p /usr/local/cuda-10.0/nccl/lib
-# sudo ln -s /usr/lib/x86_64-linux-gnu/libnccl.so.2 /usr/local/cuda/nccl/lib/
-# sudo ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so.7 /usr/local/cuda-10.0/lib64/
-
-echo 'export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}' >> ~/.bashrc
-echo 'If everything worked fine, reboot now.'
+wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_linux-run
+sudo bash cuda_8.0.44_linux-run --silent --verbose --toolkit --samples --override --no-opengl-libs
+chmod +x cuda_8.0.44_linux-run
+./cuda_8.0.44_linux-run --tar mxvf
+sudo bash cuda_8.0.61_375.26_linux-run --silent --verbose --toolkit --samples --override --no-opengl-libs
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
+export PATH=/usr/local/cuda-8.0/bin:$PATH
