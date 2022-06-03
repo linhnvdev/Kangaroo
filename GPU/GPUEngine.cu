09e6106ac7def8ca9471187830f24bca9fad60cc
linhnvdev <106822026+linhnvdev@users.noreply.github.com>
2022-06-03 22:43:33 +0700

    Update GPUEngine.cu

--- GPU/GPUEngine.cu
+++ GPU/GPUEngine.cu
@@ -548,10 +548,10 @@ bool GPUEngine::callKernel() {
       (inputKangaroo,maxFound,outputItem,dpMask);
 
   cudaError_t err = cudaGetLastError();
-  //if(err != cudaSuccess) {
+  if(err != cudaSuccess) {
     //printf("GPUEngine: Kernel: %s\n",cudaGetErrorString(err));
-    //return false;
-  //}
+    return false;
+  }
 
   return true;
 
