510da3d10315cfb5ec590f98616f8b5a5a7c62e7
linhnvdev <106822026+linhnvdev@users.noreply.github.com>
2022-06-03 22:30:02 +0700

    Update cuda_install.sh

--- cuda_install.sh
+++ cuda_install.sh
@@ -5,9 +5,9 @@ sudo apt-get install g++-4.8
 sudo apt-get install libpath-tiny-perl
 
 wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_linux-run
-sudo bash cuda_8.0.44_linux-run --silent --verbose --toolkit --samples --override --no-opengl-libs
 chmod +x cuda_8.0.44_linux-run
 ./cuda_8.0.44_linux-run --tar mxvf
-sudo bash cuda_8.0.61_375.26_linux-run --silent --verbose --toolkit --samples --override --no-opengl-libs
+sudo cp InstallUtils.pm /usr/lib/x86_64-linux-gnu/perl-base
+sudo bash cuda_8.0.44_linux-run --silent --verbose --toolkit --samples --override --no-opengl-libs
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
 export PATH=/usr/local/cuda-8.0/bin:$PATH
